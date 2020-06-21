fc7eff28d83fff4da6e65e01acdff7bba1aa956f
Tao <66189688+tao-sun2@users.noreply.github.com>
2020-06-21 17:59:07 +0800

    Add files via upload

---
+++ pipelines.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+from scrapy.exporters import JsonItemExporter
+
+
+class DemoPipeline:
+
+    def __init__(self):
+        self.fp = open('t.json', 'wb')
+        self.exporter = JsonItemExporter(self.fp, ensure_ascii=False, encoding='utf-8')
+        self.exporter.start_exporting()
+
+    def open_spider(self, spider):
+        print("start")
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.fp.close()
+        print("close")
